e552d993a3aeb3dcfff804dc7327da8ba24a8afe
Darian Tavana <datavana@gmail.com>
2019-10-31 15:38:57 -0400

    Add registeredAt to Mongo

--- src/models/Registration.ts
+++ src/models/Registration.ts
@@ -3,16 +3,21 @@ import {Document, Model, model, Schema} from "mongoose";
 interface IRegistration extends Document {
     discordId: string;
     steamId: string;
+    steamName: string;
+    discordName: string;
+    steamAvatar: string;
+    discordAvatar: string;
     registeredAt: Date;
 }
 
 const RegistrationSchema: Schema = new Schema({
     discordId: { type: String, required: true, unique: true },
     steamId: { type: String, required: true, unique: true },
-    steamName: { type: String, required: true, unique: true },
-    discordName: { type: String, required: true, unique: true },
-    steamAvatar: { type: String, required: false, unique: true },
-    discordAvatar: { type: String, required: false, unique: true },
+    steamName: { type: String, required: true, unique: false },
+    discordName: { type: String, required: true, unique: false },
+    steamAvatar: { type: String, required: false, unique: false },
+    discordAvatar: { type: String, required: false, unique: false },
+    registeredAt: { type: Date, required: true, unique: false },
 });
 
 export const Registration: Model<IRegistration> = model<IRegistration>("Registration", RegistrationSchema);
