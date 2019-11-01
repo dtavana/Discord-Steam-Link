5d0c8c4be83a171e227f1e10bc434b0a8fceba32
Darian Tavana <datavana@gmail.com>
2019-10-31 21:11:29 -0400

    Update result of registrations endpoint

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -1,7 +1,13 @@
 import {Registration} from "../models/Registration";
 
 export const registrations = async(req, res) => {
-    const data = await Registration.find({}, { 'steamId': 1, '_id': 0 });
-    res.status(200).send({data});
+    const players = await Registration.find({}, { 'steamId': 1, '_id': 0 });
+    const playersReduced = players.map(player => player.steamId);
+    const result = {
+        data: {
+            players: playersReduced
+        }
+    };
+    res.status(200).send(result);
 };
 
